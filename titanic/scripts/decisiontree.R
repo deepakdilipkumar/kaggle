0a774dd86dd7f88cb815a57326d943786c01b5de
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-28 21:11:30 +0530

    Adding decision tree visualization using rattle

--- titanic/scripts/decisiontree.R
+++ titanic/scripts/decisiontree.R
@@ -1,13 +1,18 @@
 library(ggplot2)
 library(rpart)
+#library(rattle)
 
 train <- read.csv("..//data//train.csv")
 test <- read.csv("..//data//test.csv")
 
 basic_tree <- rpart(formula = Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method = "class")
-prediction <- predict(basic_tree, test, type="class")
+prediction <- predict(basic_tree, newdata=test, type="class")
+
+#pdf(file="..//output//decisiontree.pdf")
+#fancyRpartPlot(basic_tree)
+#dev.off()
 
 solution <- data.frame(PassengerId = test$PassengerId , Survived = prediction)
 
-write.csv(solution,"..//data//decisiontreemodel.csv",row.names=FALSE)
+write.csv(solution,"..//output//decisiontreemodel.csv",row.names=FALSE)
 table(prediction)
