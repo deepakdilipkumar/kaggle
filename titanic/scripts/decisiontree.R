99800a99481fca0e0105da7a11bf9f4d6ee63661
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-28 20:55:58 +0530

    Correcting predict() syntax

--- titanic/scripts/decisiontree.R
+++ titanic/scripts/decisiontree.R
@@ -4,9 +4,10 @@ library(rpart)
 train <- read.csv("..//data//train.csv")
 test <- read.csv("..//data//test.csv")
 
-basic_tree <- rpart(formula = Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, model = "class")
-prediction <- predict(basic_tree, data=test, model="class")
+basic_tree <- rpart(formula = Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method = "class")
+prediction <- predict(basic_tree, test, type="class")
 
 solution <- data.frame(PassengerId = test$PassengerId , Survived = prediction)
 
+write.csv(solution,"..//data//decisiontreemodel.csv",row.names=FALSE)
 table(prediction)
