d5a312eefe8c5184be49e937621fdb6fdc209a48
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-29 21:46:29 +0530

    Age exploration, histogram

--- titanic/scripts/explorepandas.py
+++ titanic/scripts/explorepandas.py
@@ -1,7 +1,16 @@
 import pandas as pd 
+import pylab as p
 
 dftrain = pd.read_csv("..//data//train.csv",header=0)
 dftest = pd.read_csv("..//data//test.csv",header=0)
 
 print(dftrain.describe())
 print(dftrain.info())
+
+# Old Passengers
+
+old = dftrain[dftrain.Age>60][["Age","Pclass","Sex","Survived"]]
+print(old)
+
+dftrain.Age.hist()
+p.show()
