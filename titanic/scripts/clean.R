8f1344f40de9a06be679ae52bafa8e169dc5dffc
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-30 22:11:36 +0530

    Imputing missing age values using decision tree

--- titanic/scripts/clean.R
+++ titanic/scripts/clean.R
@@ -1,3 +1,5 @@
+library(rpart)
+
 train <- read.csv("..//data//train.csv",colClasses=c("integer","factor","factor","character","factor","numeric","integer","integer","character","numeric","character","factor"))
 test <- read.csv("..//data//test.csv",colClasses=c("integer","factor","character","factor","numeric","integer","integer","character","numeric","character","factor"))	
 
@@ -18,8 +20,7 @@ for (i in 1:12) {
 #PassengerId Survived Pclass               Name  Sex  Age SibSp Parch Ticket Fare Cabin Embarked
 #1044        0      3 Storey, Mr. Thomas male 60.5     0     0   3701   NA              S
 
-temp$Faref <- temp$Fare
-temp$Faref[1044] <- median(temp$Fare[!is.na(temp$Fare) & temp$Pclass==3 & temp$Sex=="male" & temp$Embarked=="S"])
+temp$Fare[1044] <- median(temp$Fare[!is.na(temp$Fare) & temp$Pclass==3 & temp$Sex=="male" & temp$Embarked=="S"])
 
 # 2 missing values in train set for Passengers 62 and 830 - Embarked
 
@@ -27,9 +28,14 @@ table(temp$Embarked[temp$Sex=="female" & temp$Pclass==1])
 
 # Most female passengers in first class were either from C or S. Records indicate Southampton
 
-temp$Embarkedf <- temp$Embarked
-temp$Embarkedf[c(62,830)] <- c("S","S")
+temp$Embarked[c(62,830)] <- c("S","S")
+
+# 263 missing values in Age
+
+simpleTree <- rpart(formula = Age ~ Sex + Pclass + SibSp + Parch + Fare + Embarked, data=temp)
+agePrediction <- predict(simpleTree, newdata=temp[is.na(temp$Age),])
+
+temp$Age[is.na(temp$Age)] <- agePrediction
 
 #train <- temp[1:l1,]
 #test <- temp[l3:l4,]
-
