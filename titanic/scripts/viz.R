81058f55a8de5197a7d47e2564c8282fe0ae5937
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-03 21:22:18 +0530

    Updating gender viz

--- titanic/scripts/viz.R
+++ titanic/scripts/viz.R
@@ -3,5 +3,5 @@ library(ggplot2)
 input = read.csv("..//data//cleantrain.csv")
 
 png("..//output//GenderSurvived.png",width=800,height=700)
-ggplot(input,aes(x=Sex))+geom_bar(stat="count",aes(fill=factor(Survived)))
+ggplot(input,aes(x=factor(Survived)))+geom_bar(stat="count",aes(fill=Sex))
 dev.off()
