0856f82ef71d6c29d9ad6d4b94537d0f81ef7b5c
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-03 22:53:47 +0530

    Correcting write csv issue

--- titanic/scripts/svmmodel.R
+++ titanic/scripts/svmmodel.R
@@ -1,6 +1,12 @@
-library(svm)
+library(e1071)
 library(rpart)
 
 train <- read.csv("..//data//cleantrain.csv",colClasses=c("integer","factor","factor","character","factor","numeric","integer","integer","character","numeric","character","factor","factor","factor","factor","integer"))
 test <- read.csv("..//data//cleantest.csv",colClasses=c("integer","factor","character","factor","numeric","integer","integer","character","numeric","character","factor","factor","factor","factor","integer"))	
 
+svmmodel <- svm(Survived ~ Gender + Pclass + Age + Location + Fare + FamilySize, data=train)
+prediction <- predict(svmmodel,newdata=test, type="class")
+
+solution <- data.frame(PassengerId=test$PassengerId, Survived=prediction)
+
+write.csv(solution,"..//output//svmwithfamilysize.csv",row.names=FALSE)
