2c0b89ec63cfe235f2ee1c6bef9486d5374e539d
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-03 21:23:37 +0530

    Adding Passenger class viz

--- titanic/scripts/viz.R
+++ titanic/scripts/viz.R
@@ -5,3 +5,7 @@ input = read.csv("..//data//cleantrain.csv")
 png("..//output//GenderSurvived.png",width=800,height=700)
 ggplot(input,aes(x=factor(Survived)))+geom_bar(stat="count",aes(fill=Sex))
 dev.off()
+
+png("..//output//PclassSurived.png",width=800,height=700)
+ggplot(input,aes(x=factor(Survived)))+geom_bar(stat="count",aes(fill=factor(Pclass)))
+dev.off()
