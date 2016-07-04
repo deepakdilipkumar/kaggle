eff400b0261fcc9966e960d6bc8833ba20a7f0ee
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-04 21:22:53 +0530

    SVM Model using all available and generated features

--- titanic/scripts/svmmodel.R
+++ titanic/scripts/svmmodel.R
@@ -4,9 +4,9 @@ library(rpart)
 train <- read.csv("..//data//cleantrain.csv",colClasses=c("integer","factor","factor","character","factor","numeric","integer","integer","character","numeric","character","factor","factor","factor","factor","integer"))
 test <- read.csv("..//data//cleantest.csv",colClasses=c("integer","factor","character","factor","numeric","integer","integer","character","numeric","character","factor","factor","factor","factor","integer"))	
 
-svmmodel <- svm(Survived ~ Gender + Pclass + Age + Location + Fare + FamilySize, data=train)
+svmmodel <- svm(Survived ~ Gender + Pclass + AgeCategory + Fare + FamilySize + Age + Parch + SibSp + Location , data=train, type="C-classification", kernel="radial")
 prediction <- predict(svmmodel,newdata=test, type="class")
 
 solution <- data.frame(PassengerId=test$PassengerId, Survived=prediction)
 
-write.csv(solution,"..//output//svmwithfamilysize.csv",row.names=FALSE)
+write.csv(solution,"..//output//svmwithallfeatures.csv",row.names=FALSE)
