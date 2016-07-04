4d9711268319f9efe5ea458c20672a67b3734d0a
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-04 21:41:50 +0530

    Adding cross validation to find best gamma

--- titanic/scripts/svmmodel.R
+++ titanic/scripts/svmmodel.R
@@ -4,6 +4,30 @@ library(rpart)
 train <- read.csv("..//data//cleantrain.csv",colClasses=c("integer","factor","factor","character","factor","numeric","integer","integer","character","numeric","character","factor","factor","factor","factor","integer"))
 test <- read.csv("..//data//cleantest.csv",colClasses=c("integer","factor","character","factor","numeric","integer","integer","character","numeric","character","factor","factor","factor","factor","integer"))	
 
+cvparam = 10
+n=dim(train)[1]
+k=trunc(n/cvparam)
+
+errormatrix = data.frame()
+#names(errormatrix)=c("Gamma","Cost","Eval")
+
+for (g in 1:1000){
+  errorval =0
+  for (i in 1:10) {
+    lower <- k*(i-1)+1
+    upper <- k*(i)
+    cvtrain <- train[-(lower:upper),]
+    cvvalidate <- train[lower:upper,]
+    cvsvmmodel <- svm(Survived ~ Gender + Pclass + AgeCategory + Fare + FamilySize + Age + Parch + SibSp + Location , data=cvtrain, type="C-classification", kernel="radial",gamma=g/1000)
+    cvprediction <- predict(cvsvmmodel,newdata=cvvalidate, type="class")
+    wrongpredictions <- cvprediction!=cvvalidate$Survived
+    errorval <- errorval + length(cvvalidate$Survived[wrongpredictions])/length(cvvalidate$Survived)
+  }
+  errorval <- errorval/cvparam
+  errormatrix <- rbind(errormatrix,c(g/1000,errorval))
+  print(g)
+}
+
 svmmodel <- svm(Survived ~ Gender + Pclass + AgeCategory + Fare + FamilySize + Age + Parch + SibSp + Location , data=train, type="C-classification", kernel="radial")
 prediction <- predict(svmmodel,newdata=test, type="class")
 
